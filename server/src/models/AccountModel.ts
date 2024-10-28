41c7b8ffa7a0aa4e7a7e4964fc0cd410ae9171e0
czrsd <marco077x@gmail.com>
2024-10-28 20:34:27 +0100

    Add first model

---
+++ server/src/models/AccountModel.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+interface IAccount extends Document {
+    username: string;
+    password?: string;
+    imageURL: string;
+    role: string;
+    bio?: string;
+    badges: string[];
+    online: boolean;
+    lastOnline: Date;
+    visible: boolean;
+    create_time: Date;
+}
+
+const modAccountSchema: Schema<IAccount> = new mongoose.Schema({
+    username: { type: String, required: true },
+    password: { type: String, required: false },
+    imageURL: { type: String, required: true },
+    role: { type: String, required: true },
+    bio: { type: String, required: false },
+    badges: { type: [String], required: true },
+    online: { type: Boolean, required: true },
+    lastOnline: { type: Date, default: Date.now },
+    visible: { type: Boolean, required: true },
+    create_time: { type: Date, default: Date.now },
+});
+
+const AccountModel = mongoose.model<IAccount>('moduser', modAccountSchema);
+
+export default AccountModel;

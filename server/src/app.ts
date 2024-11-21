17694ff55fb94c108977fc6f8a03e61829ec13a9
Marco <marco077x@gmail.com>
2024-11-21 22:29:54 +0100

    Add express useragent and express rate limit

--- server/src/app.ts
+++ server/src/app.ts
@@ -2,6 +2,8 @@ import express, { Application } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import helmet from 'helmet';
+import useragent from 'express-useragent';
+import rateLimit from 'express-rate-limit';
 
 const SetupMiddleware = (app: Application) => {
     app.use(
@@ -18,6 +20,8 @@ const SetupMiddleware = (app: Application) => {
         })
     );
 
+    app.use(useragent.express());
+
     app.use(express.json());
     app.use(cookieParser());
     app.use(
@@ -28,6 +32,14 @@ const SetupMiddleware = (app: Application) => {
                     scriptSrc: ["'self'", "'unsafe-inline'"],
                 },
             },
+        })
+    );
+
+    app.use(
+        rateLimit({
+            windowMs: 60 * 1000,
+            limit: 40,
+            message: 'Too many requests, please try again later.',
         })
     );
 };

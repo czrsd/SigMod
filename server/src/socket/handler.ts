182a454619f3f7503b4ffe34b23a7b0feef8962c
Marco <marco077x@gmail.com>
2024-11-23 16:27:59 +0100

    Add method to get server specific sockets

--- server/src/socket/handler.ts
+++ server/src/socket/handler.ts
@@ -15,6 +15,19 @@ class WebSocketHandler {
         this.modLink =
             'https://update.greasyfork.org/scripts/454648/SigMod%20Client%20%28Macros%29.user.js';
     }
+
+    getServerSockets(server: string) {
+        const allSockets = Array.from(this.sockets.values());
+        const serverSockets: socket[] = [];
+
+        allSockets.forEach((socket) => {
+            if (socket.server === server) {
+                serverSockets.push(socket);
+            }
+        });
+
+        return serverSockets;
+    }
 }
 
 export default new WebSocketHandler();

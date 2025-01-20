5ad6f992584688ba8fdb8e2b4641ce09c65685cf
Marco <marco077x@gmail.com>
2025-01-20 18:49:05 +0100

    Add method to get online sigmally users

---
+++ server/src/utils/getTotalPlayers.ts
@@ -0,0 +1,26 @@
+import axios from 'axios';
+
+const getPlayers = async () => {
+    let totalPlayers = 0;
+    const urls = [
+        'https://eu0.sigmally.com/server/serversstats',
+        'https://ca0.sigmally.com/server/serversstats',
+        'https://ca1.sigmally.com/server/serversstats',
+        'https://ca2.sigmally.com/server/serversstats',
+    ];
+
+    for (const url of urls) {
+        try {
+            const response = await axios.get(url);
+            const playersCurrent =
+                response.data.body.serverstats.players_current;
+            totalPlayers += playersCurrent;
+        } catch (error) {
+            console.error(`Failed to fetch data from ${url}:`, error);
+        }
+    }
+
+    return totalPlayers;
+};
+
+export default getPlayers;

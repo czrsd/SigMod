4109e7de66a9b14b9681f2cfc60b1a64b33f9425
Marco <marco077x@gmail.com>
2025-01-20 18:08:01 +0100

    Add Auth routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,12 +1,16 @@
 import { Application } from 'express';
-import telemetryRoutes from './telemetryRoutes';
-import announcementRoutes from './announcementRoutes';
+import telemetryRoutes from './TelemetryRoutes';
+import announcementRoutes from './AnnouncementRoutes';
 import TournamentRoutes from './TournamentRoutes';
 import FontRoutes from './FontRoutes';
+import DiscordRoutes from './DiscordRoutes';
+import FriendRoutes from './FriendRoutes';
 
 export default (app: Application) => {
     app.use(telemetryRoutes);
     app.use(announcementRoutes);
     app.use(TournamentRoutes);
     app.use(FontRoutes);
+    app.use(DiscordRoutes);
+    app.use(FriendRoutes);
 };

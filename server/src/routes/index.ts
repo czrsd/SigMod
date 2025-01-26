37da4229fdc8969d90c998504a97bc873530a489
Marco <marco077x@gmail.com>
2025-01-26 17:25:56 +0100

    Add static route for profiles

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,11 @@
-import { Application } from 'express';
+import express, { Application } from 'express';
 import telemetryRoutes from './TelemetryRoutes';
 import announcementRoutes from './AnnouncementRoutes';
 import TournamentRoutes from './TournamentRoutes';
 import FontRoutes from './FontRoutes';
 import DiscordRoutes from './DiscordRoutes';
 import FriendRoutes from './FriendRoutes';
+import path from 'path';
 
 export default (app: Application) => {
     app.use(telemetryRoutes);
@@ -13,4 +14,6 @@ export default (app: Application) => {
     app.use(FontRoutes);
     app.use(DiscordRoutes);
     app.use(FriendRoutes);
+
+    app.use(`/profiles`, express.static(path.join(process.cwd(), 'profiles')));
 };

fa4c6f0daa65aa0e0b835ea79642ef9b5fd078c0
Marco <marco077x@gmail.com>
2025-01-26 13:47:56 +0100

    remove unused jti

--- server/src/utils/jwtUtils.ts
+++ server/src/utils/jwtUtils.ts
@@ -12,11 +12,7 @@ export const generateAccessToken = (userId: string) => {
 };
 
 export const generateRefreshToken = (userId: string) => {
-    const jti = jwt.sign({}, process.env.JWT_PRIVATE_KEY || '', {
-        algorithm: 'RS256',
-        expiresIn: '1y',
-    });
-    return jwt.sign({ userId, jti }, process.env.JWT_PRIVATE_KEY || '', {
+    return jwt.sign({ userId }, process.env.JWT_PRIVATE_KEY || '', {
         algorithm: 'RS256',
         expiresIn: '1y',
     });

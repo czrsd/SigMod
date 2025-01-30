2e8ec470b769dff3f0411c8f2b881e1dd67add9e
Marco <marco077x@gmail.com>
2025-01-30 22:16:03 +0100

    Add minimapData type and refactor

--- server/src/types/socket.ts
+++ server/src/types/socket.ts
@@ -1,4 +1,11 @@
-export type SendData = {
+export type socketMessageData = {
     type: string;
     content?: any;
+};
+
+export type minimapData = {
+    x: number;
+    y: number;
+    nick: string;
+    sid: string;
 };

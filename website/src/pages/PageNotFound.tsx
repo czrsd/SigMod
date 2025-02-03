dd9de69841f439030c247e69a4c09f969ea1177b
Marco <marco077x@gmail.com>
2025-02-03 14:46:45 +0100

    refactor: replace single quotes with double quotes

--- website/src/pages/PageNotFound.tsx
+++ website/src/pages/PageNotFound.tsx
@@ -1,8 +1,10 @@
 const PageNotFound = () => {
-    return <div className='flex flex-col justify-center items-center'>
-        <h1 className=''>404 - Not found</h1>
-        <p>Sorry, this page does not exist.</p>
-        <a href='/'>Back to homepage</a>
+    return <div className="flex flex-col justify-center items-center h-screen gap-6">
+        <div className="text-center">
+            <h1 className="text-5xl font-bold">404 - Not found</h1>
+            <p className="text-lg">Sorry, this page does not exist.</p>
+        </div>
+        <a className="text-lg" href="/">Back to homepage</a>
     </div>
 };
 

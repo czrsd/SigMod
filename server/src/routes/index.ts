776cdf69c627068a3a96df7c84ff45b3ee99b281
Marco <marco077x@gmail.com>
2025-02-03 20:30:32 +0100

    Add all website-related routes

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,4 +1,4 @@
-import express, { Application } from 'express';
+import express, { Application, Request, Response } from 'express';
 import telemetryRoutes from './TelemetryRoutes';
 import announcementRoutes from './AnnouncementRoutes';
 import TournamentRoutes from './TournamentRoutes';
@@ -16,4 +16,25 @@ export default (app: Application) => {
     app.use(FriendRoutes);
 
     app.use('/profiles', express.static(path.join(process.cwd(), 'profiles')));
+
+    const websitePath = process.env.WEBSITE_PATH;
+    if (!websitePath) return;
+
+    app.use(express.static(path.join(websitePath)));
+
+    app.get('/', (req: Request, res: Response) => {
+        res.sendFile(path.join(websitePath, 'index.html'));
+    });
+
+    app.get('/changelog', (req: Request, res: Response) => {
+        res.sendFile(path.join(websitePath, 'pages', 'changelog.html'));
+    });
+
+    app.get('/tos', (req: Request, res: Response) => {
+        res.sendFile(path.join(websitePath, 'pages', 'tos.html'));
+    });
+
+    app.use((req: Request, res: Response) => {
+        res.status(404).sendFile(path.join(websitePath, 'pages', '404.html'));
+    });
 };
